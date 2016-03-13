a3b52cfa5d5c0a858bac65d89354b4d139be0cf4
bpbailey <bpbailey@illinois.edu>
2016-03-12 21:35:41 -0600

    Create Motion class

--- app/src/main/java/edu/illinois/cs498/accelerometertest/MainActivity.java
+++ app/src/main/java/edu/illinois/cs498/accelerometertest/MainActivity.java
@@ -6,51 +6,34 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-
+import java.util.Calendar;
 import java.util.List;
 
-public class MainActivity extends Activity implements SensorEventListener {
-    private SensorManager mSensorManager;
-    private Sensor mSensor;
 
+public class MainActivity extends Activity {
+    private MotionAndOrientationTracker tracker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
-
-        List deviceSensors = mSensorManager.getSensorList(Sensor.TYPE_ALL);
-        Log.d("DEBUG", deviceSensors.toString());
-
+        tracker = new MotionAndOrientationTracker(this);
     }
+
 
     protected void onResume() {
         super.onResume();
-        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR), SensorManager.SENSOR_DELAY_NORMAL);
+        tracker.onResume();
     }
 
 
     protected void onPause() {
-        mSensorManager.unregisterListener(this);
         super.onPause();
+        tracker.onPause();
     }
 
-
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-    }
-
-    public void onSensorChanged(SensorEvent se) {
-        float x = se.values[0];
-        float y = se.values[1];
-        float z = se.values[2];
-        //Log.d("D", se.toString());
-        Log.d("DEBUG", "X: " + Float.toString(x) + " Y: " + Float.toString(y) + " Z: " + Float.toString(z));
-    }
 }
+
