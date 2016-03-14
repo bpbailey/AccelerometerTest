09502d7ced90e1d249091ae454fc95da5f7480e3
bpbailey <bpbailey@illinois.edu>
2016-03-13 21:55:42 -0500

    Separate shake gesture and orientation gesture

--- app/src/main/java/edu/illinois/cs498/accelerometertest/MainActivity.java
+++ app/src/main/java/edu/illinois/cs498/accelerometertest/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
        // tracker = new MotionAndOrientationTracker(this);
-        tracker = new OrientationGestureTracker(this);
+        tracker = new OrientationGestureTracker(this, false);
     }
 
 
